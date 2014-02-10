8a93732c2db7cf038749dc32f30f2427facf906e
Marc Joffe <marc@publicsectorcredit.org>
2014-02-10 15:03:12 -0800

    Improved ggplot
    Changed r code to create more attractive plot.  Also saved dataframe in
    munitrades.rda.

--- munitrades.r
+++ munitrades.r
@@ -1,10 +1,17 @@
 library(RPostgreSQL)
 library(ggplot2)
+library(scales)
 drv <- dbDriver('PostgreSQL')
-con <- dbConnect(drv, dbname='msrb', user='postgres', password='A1b2c3d4')
+con <- dbConnect(drv, dbname='msrb', user='postgres', password='********')
 munitrades <-dbReadTable(con,'munitrades_unique')
-qplot(price_inter, data=munitrades, geom="histogram")
-attacch(munitrades)
+ggplot(data=munitrades, aes(price_inter, fill="red")) + 
+  geom_histogram(binwidth=1) + 
+  ggtitle("Number of Trades by Price") + 
+  xlab("Price") + 
+  ylab("Number of Trades") +
+  xlim(0,150) +
+  theme_bw() +
+  scale_y_continuous(labels = comma) +
+  theme(legend.position = "none")
+attach(munitrades)
 summary(price_inter)
-fivenum(price_inter)
-stem(price_inter)
